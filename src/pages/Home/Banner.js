476903705c54a7957dc6c04f8977df0b646966f3
riyaiWebSoul <riyasurena137@gmail.com>
2023-11-09 13:59:52 +0530

    todays changes

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -1,7 +1,6 @@
 import bannerVideo from "../../assets/video/banner_video.mp4";
 import bannerImage from "../../assets/images/banner_image.avif";
-import Section from "./Section";
-import { useState, useEffect,useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function Banner() {
   const [mutedButton, setMutedButton] = useState(true);
@@ -48,7 +47,7 @@ function Banner() {
         id="case_study_slider1"
         style={{
           opacity,
-          transition: "opacity 0.3s", // You can adjust the transition duration
+          transition: "opacity 0.3s",x
         }}
       >
         <div
@@ -77,27 +76,27 @@ function Banner() {
                       }}
                     />
                     <video
-          data-image={bannerImage}
-          ref={videoRef}
-          loop={true}
-          autoPlay={true}
-          muted={mutedButton}
-          style={{
-            translate: "none",
-            rotate: "none",
-            scale: "none",
-            transform: "translate3d(0px, 0px, 0px)",
-          }}
-        >
-          {shouldLoadVideo && <source src={bannerVideo} />}
-        </video>
+                      data-image={bannerImage}
+                      ref={videoRef}
+                      loop={true}
+                      autoPlay={true}
+                      muted={mutedButton}
+                      style={{
+                        translate: "none",
+                        rotate: "none",
+                        scale: "none",
+                        transform: "translate3d(0px, 0px, 0px)",
+                      }}
+                    >
+                      {shouldLoadVideo && <source src={bannerVideo} />}
+                    </video>
                   </div>
                   <button
                     aria-label="button for mute"
                     className="hlsVideoCard_controls_item__vol__YMqdq"
                     onClick={handelMute}
                   >
-                    {mutedButton? <svg
+                    {mutedButton ? <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="18.855"
                       height="18.776"
@@ -109,22 +108,22 @@ function Banner() {
                         transform="translate(15047.041 -12268.507)"
                         fill="#fff"
                       ></path>
-                    </svg>:<svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20.118"
-                    height="18.776"
-                    viewBox="0 0 20.118 18.776"
-                  >
-                    <path
-                      id="icon-vol"
-                      d="M5.193,13.694H.931A.933.933,0,0,1,0,12.763V6.634A.931.931,0,0,1,.931,5.7H5.177L12.144,0l.816.387V18.372l-.795.4Zm.663-7.258v6.5l6.1,4.45V1.442ZM1,12.694H4.855V6.7H1Zm15.644,1.779a6.955,6.955,0,0,0,.249-10.418l.68-.734a7.955,7.955,0,0,1-.285,11.916Zm-1.914-1.739a4.743,4.743,0,0,0-.035-6.744l.7-.715a5.744,5.744,0,0,1,.043,8.166Z"
-                      transform="translate(0)"
-                      fill="#fff"
-                    ></path>
-                  </svg>}
-                   
+                    </svg> : <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20.118"
+                      height="18.776"
+                      viewBox="0 0 20.118 18.776"
+                    >
+                      <path
+                        id="icon-vol"
+                        d="M5.193,13.694H.931A.933.933,0,0,1,0,12.763V6.634A.931.931,0,0,1,.931,5.7H5.177L12.144,0l.816.387V18.372l-.795.4Zm.663-7.258v6.5l6.1,4.45V1.442ZM1,12.694H4.855V6.7H1Zm15.644,1.779a6.955,6.955,0,0,0,.249-10.418l.68-.734a7.955,7.955,0,0,1-.285,11.916Zm-1.914-1.739a4.743,4.743,0,0,0-.035-6.744l.7-.715a5.744,5.744,0,0,1,.043,8.166Z"
+                        transform="translate(0)"
+                        fill="#fff"
+                      ></path>
+                    </svg>}
+
                   </button>
-                  
+
                 </div>
                 <div className="newBanner_banner_inner__B_pMf newBanner_full__kYor7">
                   <div className="container">
@@ -166,13 +165,13 @@ function Banner() {
               </div>
             </div>
           </div>
-           
+
         </div>
-         
+
       </section>
     </div>
-     
-  
+
+
   );
 }
 
---
+++ src/pages/Home/Services.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useRef, useState } from "react";
+
+function Services() {
+ 
+
+  return (
+  <div>
+    <div></div>
+  </div>
+  );
+}
+
+export default Services;
